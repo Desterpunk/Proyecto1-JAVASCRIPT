b44aae2b6a5f91544d78c98f449e2269e2d763b3
Jhon Serna <jhonalfaber@gmail.com>
2021-09-06 20:21:19 -0500

    Añadiento Factura.js]

---
+++ src/Factura.js
@@ -0,0 +1,37 @@
+class Factura{
+    constructor(){
+        this.arrayOpcion1 = new Array(0);
+        this.arrayOpcion2 = new Array(0);
+        this.arrayOpcion3 = new Array(0);
+    }
+    llenarFactura(objeto,producto,inventario){
+        switch (producto) {
+            case "electrodomesticos":
+                if(inventario.arrayOpcion1.length > 0){
+                    this.arrayOpcion1.push(objeto);
+                    break;
+                }
+                break;
+            case "neveras":
+                if(inventario.arrayOpcion2.length > 0){
+                    this.arrayOpcion2.push(objeto);
+                    break;
+                }
+                break;
+            case "televisores":
+                if(inventario.arrayOpcion3.length > 0){
+                    this.arrayOpcion3.push(objeto);
+                    break;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    limpiarFactura(){
+        this.arrayOpcion1 = new Array(0);
+        this.arrayOpcion2 = new Array(0);
+        this.arrayOpcion3 = new Array(0);
+    }
+}
